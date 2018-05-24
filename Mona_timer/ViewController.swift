4eb6c878d8523302f9276f1bd49adfcf758c40bd
Ryu <pythonlove220@gmail.com>
2018-05-24 09:38:17 +0900

    add first files

---
+++ Mona_timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mona_timer
+//
+//  Created by 石橋龍 on 2018/05/24.
+//  Copyright © 2018 Team snake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
